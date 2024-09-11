545b1f203bf690fa7e9f935aa316277e39a36b89
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-09-11 12:03:40 +0500

    try fix icon load issue and add missing icons for brush stroke width

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,12 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
+  icon: {
+    clientBundle: {
+      scan: true,
+    }
+  },
+
   modules: ["@nuxt/eslint", "@nuxt/fonts", "@nuxt/ui"],
   fonts: {
     families: [

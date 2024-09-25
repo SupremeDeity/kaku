bc616c1ba435fd435974d5886be30c59eee10f6a
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-09-25 16:25:06 +0500

    fix: version commit sha

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  
+   runtimeConfig: {
+    public: {
+      commitSha: process.env.VERCEL_GIT_COMMIT_SHA?.slice(0, 7) ?? "DEV"
+    }
+  },
 
   icon: {
     clientBundle: {

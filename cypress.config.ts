388e0143a0f448b18ffecbd0bec7aff287be2a5c
SupremeDeity <sheikhmohsin706@gmail.com>
2025-07-21 23:11:36 +0500

    remove unnessary cypress support file

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,14 +3,11 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    supportFile: 'tests/e2e/support/e2e.ts',
+    supportFile: false,
     specPattern: 'tests/e2e/**/*.cy.ts',
     viewportWidth: 1280,
     viewportHeight: 720,
     video: false,
     screenshotOnRunFailure: true,
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
   },
 })
